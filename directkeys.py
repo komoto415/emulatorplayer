199f9c115325019e06e50dfb8d3be474912b5674
Jeffrey Ng <komoto415@gmail.com>
2020-05-07 22:40:18 -0500

    dynamically generate whitelist based on the device

--- directkeys.py
+++ directkeys.py
@@ -23,7 +23,6 @@ SPACE = 0x39
 # C struct redefinitions
 PUL = ctypes.POINTER(ctypes.c_ulong)
 
-
 class KeyBdInput(ctypes.Structure):
     _fields_ = [("wVk", ctypes.c_ushort),
                 ("wScan", ctypes.c_ushort),
@@ -31,12 +30,10 @@ class KeyBdInput(ctypes.Structure):
                 ("time", ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
 
-
 class HardwareInput(ctypes.Structure):
     _fields_ = [("uMsg", ctypes.c_ulong),
                 ("wParamL", ctypes.c_short),
                 ("wParamH", ctypes.c_ushort)]
-
 
 class MouseInput(ctypes.Structure):
     _fields_ = [("dx", ctypes.c_long),
@@ -46,17 +43,14 @@ class MouseInput(ctypes.Structure):
                 ("time", ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
 
-
 class Input_I(ctypes.Union):
     _fields_ = [("ki", KeyBdInput),
                 ("mi", MouseInput),
                 ("hi", HardwareInput)]
 
-
 class Input(ctypes.Structure):
     _fields_ = [("type", ctypes.c_ulong),
                 ("ii", Input_I)]
-
 
 # Actuals Functions
 
@@ -67,14 +61,12 @@ def PressKey(hexKeyCode):
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-
 def ReleaseKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
     ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
 
 if __name__ == '__main__':
     PressKey(0x11)
--- keylogger.py
+++ keylogger.py
@@ -2,52 +2,51 @@ from pynput.keyboard import Key, Listener
 from datetime import datetime
 import os
 
+# Logistics sake for file naming
+NOW = datetime.isoformat(datetime.now())
+NOW = NOW.replace('T', '-').replace(':', '-')[:-4]
+
+# What device we will be whitelisting keys for
+DEVICE = "GBA"
 
 # We will only record these keys because they are the only ones that matter to the emulators
-keys_whitelist = [
-    'W', 'A', 'S', 'D', 'Z', 'X',
-    "BACKSPACE", "ENTER", "SPACE",
-]
+# For some scalability if we decided to branch out to other games, we'll just have a simple getter for the list of keys
+# that will be whitelisted based on the keymap configuration for any particular device, as determined by our DEVICE
+# constant
+def get_key_whitelist():
+    whitelist = {
+        "GBA": [
+            'W', 'A', 'S', 'D', 'Z', 'X',
+            "BACKSPACE", "ENTER", "SPACE",
+        ],
+        "DS": [
 
-# Character count just to know when to newline our text file just to help some readability. Might remove to simplify
-# file reading of the emulator player if the newline character makes is more difficult
-char_count = 0
-now = datetime.isoformat(datetime.now())
-now = now.replace('T', '-').replace(':', '-')[:-4]
-
+        ],
+    }
+    return whitelist.get(DEVICE)
 
 def on_press(key):
     # Update the text file for each key pressed
     write_to_file(key)
-
 
 # Escape key will kill the key logger script
 def on_release(key):
     if key is Key.esc:
         return False
 
-
 def write_to_file(key):
-    global char_count, now
-
-    with open(os.path.join("./logs", f"logger_{now}.txt"), "a") as f:
+    with open(os.path.join("./logs", f"logger_{NOW}.txt"), "a") as f:
         trimmed_key = str(key).replace("'", "").upper()
 
         # Trimming the string of the special key cases
         if "KEY." in trimmed_key:
             trimmed_key = trimmed_key[4:]
 
-        if trimmed_key in keys_whitelist:
+        if trimmed_key in get_key_whitelist():
+            # For the sake ease when using Python's native readlines function, either each line is the key pressed or
+            # have it all recorded in one line separated by spaces. Doesn't matter which one, just picked on arbitrarily
             f.write(trimmed_key)
-            char_count += len(trimmed_key)
-
-            if char_count > 100 and char_count is not 0:
-                f.write('\n')
-                char_count = 0
-            else:
-                f.write(' ')
-                char_count += 1
-
+            f.write('\n')
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
